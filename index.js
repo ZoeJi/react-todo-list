2d398862d78a7e440559cc3433e7a84a076ab918
ZoeJi <zoe.q.ji@gmail.com>
2019-04-09 14:34:11 -0700

    added onDelete with index

--- index.js
+++ index.js
@@ -10,13 +10,21 @@ class App extends Component {
     super();
     this.state = {
       todos: [
-        {id: 1, text: 'first'},
-        {id: 2, text: 'second'},
-        {id: 3, text: 'third'}
+        {id: 1, text: 'clean kitch'},
+        {id: 2, text: 'take shower'},
+        {id: 3, text: 'pick up package'}
       ]
     };
   }
 
+  handleDelete = (index) => {
+    this.setState({
+      todos: [
+        ...this.state.todos.slice(0,index),
+        ...this.state.todos.slice(index + 1)
+      ]
+    })
+  }
   handleAdd = (text) => {
     this.setState({
       todos: [
@@ -32,7 +40,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <TodoList todos={this.state.todos}/>
+        <TodoList todos={this.state.todos} handleDelete={this.handleDelete}/>
         <UserInput handleAdd={this.handleAdd}/>
       </div>
     );
@@ -44,10 +52,10 @@ class App extends Component {
  */
 class TodoList extends Component {
   render() {
-    const { todos } = this.props;
+    const { todos, handleDelete } = this.props;
     return (
       <div> 
-        {todos.map(({id, text}) => <TodoItem key={id} text={text} />)}
+        {todos.map(({id, text}, index) => <TodoItem key={id} index={index} text={text} handleDelete={handleDelete}/>)}
       </div>
     )
   }
@@ -70,6 +78,10 @@ class TodoItem extends Component {
     }));
   }
 
+  onDelete = () => {
+    this.props.handleDelete(this.props.index);
+  }
+
   render() {
     const textStyle = {
       'textDecoration': this.state.done ? 'line-through' : 'none'
@@ -78,12 +90,15 @@ class TodoItem extends Component {
       <div> 
         <button className="done-btn" onClick={this.onClick}>{this.state.done ? 'undo' : 'done'}</button>
         <span className="todo-text" style={textStyle}>{this.props.text}</span>
-        <button className="delete-btn"> x </button> 
+        <button className="delete-btn" onClick={this.onDelete}> x </button> 
       </div>
     )
   }
 }
 
+/**
+ * UserInput
+ */
 class UserInput extends Component {
   constructor(props) {
     super(props);
